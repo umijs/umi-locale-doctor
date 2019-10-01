112f9f0423285b1361073bb98e24b71b18c29378
Howard.Zuo <leftstick@qq.com>
2019-10-01 19:49:58 +0800

    test: more cases for object.ts

--- test/helpers/object.test.ts
+++ test/helpers/object.test.ts
@@ -3,3 +3,11 @@ import { flatten } from '@/src/helpers/object'
 test('multilevel-dimensional array', () => {
   expect(flatten<number>([1, [2], [[3]]])).toEqual([1, 2, 3])
 })
+
+test('null val', () => {
+  expect(flatten<number>(null)).toEqual([])
+})
+
+test('invalid val', () => {
+  expect(flatten<number>('hello')).toEqual([])
+})
